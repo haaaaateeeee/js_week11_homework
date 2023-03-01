181eb75991f1ed40965c31e32fbceeee80d46332
haaaaateeeee <duninaalena94@gmail.com>
2023-03-01 22:31:02 +0300

    2nd_commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-let price = document.querySelector('.disc-price').textContent;
-let totalPrice = document.querySelector('.disc-price');
-let button = document.querySelector('.button');
-let discount = price / 100 * 20
+let price1 = document.querySelectorAll('.price')[0].innerHTML;
+let price2 = document.querySelectorAll('.price')[1].innerHTML;
+let price3 = document.querySelectorAll('.price')[2].innerHTML;
+let price4 = document.querySelectorAll('.price')[3].innerHTML;
+
+let sum = [Number(price1) + Number(price2) + Number(price3) + Number(price4)];
+
+document.getElementById('finalPrice').textContent = sum;
+
+let finalPrice = document.querySelector('.final-price')
 
 function discountPrice() {
-    let result = Number(price) - discount;
-    totalPrice.textContent = result;
+    let result = Number(sum) * 0.8;
+    finalPrice.textContent = result;
 }
 
-button.addEventListener('click', changePrice);
